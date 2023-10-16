1cea6d708e057e85e5278321786d139696533720
Jefferson GT <jeferguto1@outlook.com>
2023-10-16 15:41:18 -0500

    comments

--- index.js
+++ index.js
@@ -1,65 +1,83 @@
-// index.js
+// Crear nuevos Web Workers para los cronómetros
 const worker1 = new Worker("worker.js");
 const worker2 = new Worker("worker.js");
 const worker3 = new Worker("worker.js");
 
+// Variables para almacenar los segundos de cada cronómetro
 var seg1 = 0;
 var seg2 = 0;
 var seg3 = 0;
 
 // Funciones para comunicarse con los Workers
 function iniciarCronometro(worker, intervalo, segmento) {
+  // Enviar un mensaje al Web Worker para iniciar el cronómetro
   worker.postMessage({ action: "iniciar", intervalo });
-  segmento = 0;
+  segmento = 0; // Restablecer el segmento del cronómetro
 }
 
 function pausarCronometro(worker, segmento) {
+  // Enviar un mensaje al Web Worker para pausar el cronómetro
   worker.postMessage({ action: "pausar" });
-  segmento = 0;
+  segmento = 0; // Restablecer el segmento del cronómetro
 }
 
 function continuarCronometro(worker, intervalo, segmento) {
+  // Enviar un mensaje al Web Worker para continuar el cronómetro
   worker.postMessage({ action: "continuar", intervalo });
 }
 
 function terminarCronometro(worker, segmento) {
+  // Enviar un mensaje al Web Worker para terminar el cronómetro
   worker.postMessage({ action: "terminar" });
-  segmento = 0;
+  segmento = 0; // Restablecer el segmento del cronómetro
 }
 
 // Manejar mensajes del Worker
 worker1.onmessage = function (event) {
+  // Actualizar la variable seg1 con los segundos del Web Worker
   seg1 = event.data;
+  // Actualizar el elemento de la interfaz con el tiempo convertido a minutos:segundos
   document.getElementById("cronometro1").textContent = convertirASegundos(seg1);
 };
 
 worker2.onmessage = function (event) {
+  // Actualizar la variable seg2 con los segundos del Web Worker
   seg2 = event.data;
+  // Actualizar el elemento de la interfaz con el tiempo convertido a minutos:segundos
   document.getElementById("cronometro2").textContent = convertirASegundos(seg2);
 };
 
 worker3.onmessage = function (event) {
+  // Actualizar la variable seg3 con los segundos del Web Worker
   seg3 = event.data;
+  // Actualizar el elemento de la interfaz con el tiempo convertido a minutos:segundos
   document.getElementById("cronometro3").textContent = convertirASegundos(seg3);
 };
 
 // Manejar eventos de los botones
+
+// Cronómetro 1
 document.getElementById("iniciar1").addEventListener("click", () => {
+  // Llamar a la función para iniciar el cronómetro 1 con un intervalo de 1 segundo
   iniciarCronometro(worker1, 1, seg1);
 });
 
 document.getElementById("terminar1").addEventListener("click", () => {
+  // Llamar a la función para terminar el cronómetro 1
   terminarCronometro(worker1, seg1);
 });
 
 document.getElementById("pausar1").addEventListener("click", () => {
+  // Llamar a la función para pausar el cronómetro 1
   pausarCronometro(worker1, seg1);
 });
 
 document.getElementById("continuar1").addEventListener("click", () => {
+  // Llamar a la función para continuar el cronómetro 1 con un intervalo de 1 segundo
   continuarCronometro(worker1, 1, seg1);
 });
 
+// Cronómetro 2 (Los comentarios para los botones del cronómetro 2 son similares a los del cronómetro 1)
 document.getElementById("iniciar2").addEventListener("click", () => {
   iniciarCronometro(worker2, 3, seg2);
 });
@@ -76,6 +94,7 @@ document.getElementById("continuar2").addEventListener("click", () => {
   continuarCronometro(worker2, 3, seg2);
 });
 
+// Cronómetro 3 (Los comentarios para los botones del cronómetro 3 son similares a los del cronómetro 1)
 document.getElementById("iniciar3").addEventListener("click", () => {
   iniciarCronometro(worker3, 6, seg3);
 });
@@ -94,11 +113,13 @@ document.getElementById("continuar3").addEventListener("click", () => {
 
 // Botón para pausar todos los cronómetros
 document.getElementById("pausarTodos").addEventListener("click", () => {
+  // Llamar a la función para pausar todos los cronómetros
   pausarCronometro(worker1, seg1);
   pausarCronometro(worker2, seg2);
   pausarCronometro(worker3, seg3);
 });
 
+// Función para convertir segundos totales en minutos:segundos
 function convertirASegundos(segundosTotales) {
   const minutos = Math.floor(segundosTotales / 60);
   const segundos = segundosTotales % 60;
--- worker.js
+++ worker.js
@@ -1,59 +1,36 @@
+// Declara las variables para el intervalo y el ID del intervalo.
 let intervalId;
 let interval;
 
-
+// Escucha mensajes del hilo principal.
 self.addEventListener('message', (event) => {
-    if (event.data.action === 'iniciar') {
-        interval = 0;
-        console.log("interval = " + interval);
-        intervalId = setInterval(() => {
-            interval += event.data.intervalo;
-            self.postMessage(interval);
-        }, 1000);
-    } else if (event.data.action === 'pausar') {
-        clearInterval(intervalId);          
-    } else if (event.data.action === 'continuar') {
-        intervalId = setInterval(() => {
-            interval += event.data.intervalo;
-            self.postMessage(interval);
-        }, 1000);
-    } else if (event.data.action === 'terminar') {
-        clearInterval(intervalId);
-        interval = 0;
-        self.postMessage(interval);
-    }
+  // Verifica el tipo de acción en el mensaje.
+  if (event.data.action === 'iniciar') {
+    // Inicia un cronómetro. Recibe el intervalo como parte de la acción.
+    interval = 0;
+    console.log("interval = " + interval);
+
+    // Crea un intervalo que se ejecutará cada 1000 ms (1 segundo).
+    intervalId = setInterval(() => {
+      // Incrementa el intervalo por el valor especificado en el mensaje.
+      interval += event.data.intervalo;
+      // Envía el valor del intervalo de vuelta al hilo principal.
+      self.postMessage(interval);
+    }, 1000);
+  } else if (event.data.action === 'pausar') {
+    // Pausa el cronómetro al borrar el intervalo.
+    clearInterval(intervalId);
+  } else if (event.data.action === 'continuar') {
+    // Continúa el cronómetro. Comienza un nuevo intervalo similar al inicio.
+    intervalId = setInterval(() => {
+      interval += event.data.intervalo;
+      self.postMessage(interval);
+    }, 1000);
+  } else if (event.data.action === 'terminar') {
+    // Detiene el cronómetro al borrar el intervalo y restablece el intervalo a 0.
+    clearInterval(intervalId);
+    interval = 0;
+    // Envía el valor de intervalo restablecido al hilo principal.
+    self.postMessage(interval);
+  }
 });
-
-
-/* (() => {
-
-    let hours = `00`,
-        minutes = `00`,
-        seconds = `00`,
-        chronometerDisplay = document.querySelector(`[data-chronometer]`),
-        chronometerCall
-  
-    function chronometer() {
-  
-      seconds ++
-  
-      if (seconds < 10) seconds = `0` + seconds
-  
-      if (seconds > 59) {
-        seconds = `00`
-        minutes ++
-  
-        if (minutes < 10) minutes = `0` + minutes
-      }
-  
-      if (minutes > 59) {
-        minutes = `00`
-        hours ++
-        
-        if (hours < 10) hours = `0` + hours
-      }
-  
-      chronometerDisplay.textContent = `${hours}:${minutes}:${seconds}`
-  
-    } 
-}) */
